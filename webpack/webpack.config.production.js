3b8052d80c454466aa97940a520dcf7a04ef988f
ChenJunhan <18824309984@163.com>
2019-01-24 11:47:55 +0800

    选用低版本的url-loader解决name值无效的问题和为css的url添加前缀

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -59,6 +59,7 @@ module.exports = {
       test: /\.(less|css)$/,
       use: extractCSS.extract({
         fallback: 'style-loader',
+        // publicPath: '/index/image/',          // 为生成的css里面的url添加前缀
         use: [{
           loader: 'css-loader',
         }, {
